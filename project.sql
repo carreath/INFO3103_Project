a00a6751b8227e6c2feb6e11f54973e1c4993c52
MatthewJared12 <matthew.jared15@unb.ca>
2017-11-12 17:42:45 -0400

    Update project.sql

--- project.sql
+++ project.sql
@@ -1,15 +1,34 @@
+
+Drop Table Users;
 CREATE TABLE Users (
    PersonID int NOT NULL AUTO_INCREMENT,
    Username varchar(255) NOT NULL,
    TokenP varchar(255) NOT NULL,
+   SaltyToken varchar(255) NOT NULL,
    SessionToken varchar(255),
    PRIMARY KEY (PersonID)
 );
 
 DELIMITER //
-CREATE PROCEDURE SessionUpdate(
-  IN SessionT varchar(255),
+DROP PROCEDURE IF EXISTS GetSalty;
+CREATE PROCEDURE GetSalty(
   IN usernm varchar(255)
+)
+
+  BEGIN 
+    SELECT SaltyToken FROM Users
+    WHERE Username = usernm;
+  END //
+DELIMITER ;
+
+
+
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS SessionUpdate;
+CREATE PROCEDURE SessionUpdate(
+  IN usernm varchar(255),
+  IN SessionT varchar(255)
 )
   BEGIN
 
@@ -21,19 +40,22 @@ CREATE PROCEDURE SessionUpdate(
 DELIMITER ;
 
 DELIMITER //
+DROP PROCEDURE IF EXISTS NewLogin;
 CREATE PROCEDURE NewLogin(
   IN usernm varchar(255),
-  IN token varchar(255)
+  IN token varchar(255),
+  In saltyT varchar(255)
 )
     BEGIN
-    INSERT INTO Users (Username, TokenP)
-    VALUES (usernm, token);
+    INSERT INTO Users (Username, TokenP, SaltyToken)
+    VALUES (usernm, token, saltyT);
 
     End //
 DELIMITER ;
 
 
 DELIMITER //
+DROP PROCEDURE IF EXISTS Login;
  CREATE PROCEDURE Login(
   IN usernm varchar(255),
   IN token varchar(255)
